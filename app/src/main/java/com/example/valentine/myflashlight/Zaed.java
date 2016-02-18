ac730c0534295ac5914ad99cfc26f8e9b658d954
valentinerutto <rutto28@gmail.com>
2016-02-18 12:06:16 +0300

    removed flash button

--- app/src/main/java/com/example/valentine/myflashlight/Zaed.java
+++ app/src/main/java/com/example/valentine/myflashlight/Zaed.java
@@ -20,8 +20,7 @@ public class Zaed extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
     }
